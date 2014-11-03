618230b35e1ca57b751dd4294ad14fc35ada6291
Andrey Subbotin <andrey@subbotin.me>
2014-11-04 00:02:58 +1100

    Add a createAction method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var Bacon = require('baconjs').Bacon;
 
 module.exports = {
 	createStore: function(spec) {
@@ -6,6 +7,18 @@ module.exports = {
 		_.each(_.keys(spec), function (key) {
 			result[key] = _.isFunction(spec[key]) ? spec[key]() : spec[key];
 		});
+		return result;
+	},
+
+	createAction: function (f) {
+		var result = {
+			feed: new Bacon.Bus(),
+			perform: function () {
+				var promise = f.apply(null, arguments);
+				result.feed.plug(Bacon.fromPromise(promise));
+				return promise;
+			}
+		};
 		return result;
 	}
 };
