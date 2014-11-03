9c339f05afa81f1c1e2f064919b83b9a38a4d5c2
Andrey Subbotin <andrey@subbotin.me>
2014-11-04 01:07:31 +1100

    Simplify

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 var _ = require('underscore');
 var Bacon = require('baconjs').Bacon;
 
-module.exports = {
-	createStore: function(spec) {
+function log(name, promise) {
+	promise.then(function(res) {
+		console.info('[OKAY ]: %s: %o', name, res);
+	}, function(err) {
+		console.error('[ERROR]: %s: $o', name, err);
+	});
+}
+
+function action(name, fn) {
+	var action = {
+		name: name,
+		feed: new Bacon.Bus(),
+		perform: function () {
+			var promise = fn.apply(action, arguments);
+			action.feed.plug(Bacon.fromPromise(promise));
+			if (Harukaze.debug) { 
+				log(name, promise);
+			}
+			return promise;
+		}
+	};
+	return action;
+}
+
+var Harukaze = {
+	debug: false,
+
+	store: function (spec) {
 		var result = {};
 		_.each(_.keys(spec), function (key) {
 			result[key] = _.isFunction(spec[key]) ? spec[key]() : spec[key];
@@ -10,16 +36,13 @@ module.exports = {
 		return result;
 	},
 
-	createAction: function (spec) {
-		var base = {};
-		var action = {
-			feed: new Bacon.Bus(),
-			perform: function () {
-				var promise = spec.perform.apply(base, arguments);
-				action.feed.plug(Bacon.fromPromise(promise));
-				return promise;
-			}
-		};
-		return _.extend(base, spec, action);
+	actions: function (ns, spec) {
+		var actions = {};
+		_.each(_.keys(spec), function (key) {
+			actions[key] = action(ns + '.' + key, spec[key]);
+		});
+		return actions;
 	}
 };
+
+module.exports = Harukaze;
