b531d0ad8f0d76a636e994b1aefd1a8ab79f66a9
Andrey Subbotin <andrey@subbotin.me>
2015-02-19 23:38:17 +1100

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var browserify = require('browserify');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var gutil = require('gulp-util');
-var fs = require('fs');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 
 var cfg = require('./package.json');
+
+var pureConfig = {
+  exports: capitalise(cfg.name),
+  external: {
+    lodash: {amd: 'lodash', global: '_'},
+    react: {amd: 'react', global: 'React'},
+    baconjs: {amd: 'baconjs', global: 'Bacon'}
+  }
+};
 
 function capitalise(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -22,45 +26,13 @@ gulp.task('clean', function () {
   return gulp.src('dist', { read: false }).pipe($.clean());
 });
 
-var browserifyConfig = {
-  entries: ['./index.js'],
-  debug: true,
-  standalone: capitalise(cfg.name),
-  external: ['lodash', 'react', 'baconjs'],
-  bundleExternal: false,
-};
-
 gulp.task('build', function () {
-  var bundler = browserify(browserifyConfig);
-
-  var bundle = function() {
-    return bundler
-      .bundle()
-      .pipe(source(getBundleName() + '.js'))
-      .pipe(buffer())
-      .pipe($.sourcemaps.init({loadMaps: true}))
-      .pipe($.sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist/'));
-  };
-
-  return bundle();
+  return gulp.src(cfg.main)
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.pureCjs(pureConfig))
+    .pipe($.rename({ suffix: '-' + cfg.version }))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build:minified', function () {
-  var bundler = browserify(browserifyConfig);
-
-  var bundle = function() {
-    return bundler
-      .bundle()
-      .pipe(source(getBundleName() + '.min.js'))
-      .pipe(buffer())
-      .pipe($.sourcemaps.init({loadMaps: true}))
-      .pipe($.uglify())
-      .pipe($.sourcemaps.write('./'))
-      .pipe(gulp.dest('./dist/'));
-  };
-
-  return bundle();
-});
-
-gulp.task('default', ['build', 'build:minified']);
+gulp.task('default', ['build']);
