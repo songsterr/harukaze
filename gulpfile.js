99c983aeccf009f2025d848c631042f8d8a850bc
Andrey Subbotin <andrey@subbotin.me>
2015-10-21 14:27:36 +0300

    Support CJS no longer. Use ES6.

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,6 @@ function capitalise(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-var pureConfig = {
-  exports: capitalise(cfg.name),
-  external: {
-    lodash: {amd: 'lodash', global: '_'},
-    react: {amd: 'react', global: 'React'},
-    baconjs: {amd: 'baconjs', global: 'Bacon'}
-  }
-};
-
 gulp.task('clean', function () {
   return gulp.src('dist', { read: false }).pipe($.clean());
 });
@@ -26,7 +17,6 @@ gulp.task('build', function () {
   return gulp.src(cfg.main)
     .pipe($.sourcemaps.init({loadMaps: true}))
     .pipe($.babel())
-    .pipe($.pureCjs(pureConfig))
     .pipe($.concat('harukaze.js'))
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/'));
