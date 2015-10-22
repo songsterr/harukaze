7387a06bd7565cf1b43f68349d9abe1794b2363a
Andrey Subbotin <andrey@subbotin.me>
2015-10-22 13:27:30 +0300

    Use the spread instead of the apply call.

--- src/action.js
+++ src/action.js
@@ -3,7 +3,7 @@ import Logger from './logger';
 function action(name, fn) {
   return (...args) => {
     Logger.logActionStart(name, args);
-    const promise = fn.apply(null, args);
+    const promise = fn(...args);
     Logger.logActionPromiseResult(name, promise);
     return promise;
   };
